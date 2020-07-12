74ef692729b2304f8f1eb2e5fe3b87dd3cfc75a4
Tobi <olutimehintobi@gmail.com>
2020-07-11 20:59:37 -0400

    Added the blog feature

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateField(auto_now=True)
+    date = models.DateField()
     content = models.CharField(max_length = 1000)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    return render(request, "blog/all_blogs.html")
+    blogs = Blog.objects.all().order_by('-date')[:5]
+    return render(request, "blog/all_blogs.html", {"Blogs": blogs})
