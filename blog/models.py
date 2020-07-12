6d2c47eb8dee0cd11326b8baba98f5b5cfad2898
Tobi <olutimehintobi@gmail.com>
2020-07-11 20:46:27 -0400

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    content = models.CharField(max_length = 1000)
